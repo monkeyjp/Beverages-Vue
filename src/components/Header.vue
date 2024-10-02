a028fb14a50367554c58f6571195bed09b4a68c1
Javier Pestana <javier.pestana85@gmail.com>
2024-10-02 22:38:11 +0100

    beverages store

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,14 @@
 <script setup>
-import { RouterLink } from "vue-router";
+import { computed } from "vue";
+import { RouterLink, useRoute } from "vue-router";
+
+const route = useRoute();
+
+const homePage = computed(() => route.name === "home");
 </script>
 
 <template>
-  <header class="bg-slate-800">
+  <header class="bg-slate-800" :class="{ header: homePage }">
     <div class="mx-auto container px-5 py-16">
       <div class="flex justify-between items-center">
         <div>
@@ -11,18 +16,18 @@ import { RouterLink } from "vue-router";
             <img src="/img/logo.svg" class="w-32" alt="logo" />
           </RouterLink>
         </div>
-        <nav class="flex gap-4">
+        <nav class="flex gap-4 text-white">
           <RouterLink
             :to="{ name: 'home' }"
-            class="text-white uppercase font-bold"
             active-class="text-orange-500"
+            class="uppercase font-bold"
           >
             Home
           </RouterLink>
           <RouterLink
             :to="{ name: 'favorites' }"
-            class="text-white uppercase font-bold"
             active-class="text-orange-500"
+            class="uppercase font-bold"
           >
             Favorites
           </RouterLink>
@@ -30,6 +35,7 @@ import { RouterLink } from "vue-router";
       </div>
       <form
         class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+        v-if="homePage"
       >
         <div class="space-y-4">
           <label
@@ -66,3 +72,10 @@ import { RouterLink } from "vue-router";
     </div>
   </header>
 </template>
+<style>
+.header {
+  background-image: url("/img/bg.jpg");
+  background-size: cover;
+  background-position: center;
+}
+</style>
